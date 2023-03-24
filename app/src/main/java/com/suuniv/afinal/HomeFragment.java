db6b376c6082f098558eefcc0e83d1ced237496e
harikishanbabupv <harikishanbabupv@gmail.com>
2023-03-24 18:21:06 -0500

    updated few validations

--- app/src/main/java/com/suuniv/afinal/HomeFragment.java
+++ app/src/main/java/com/suuniv/afinal/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.suuniv.afinal;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -32,7 +35,43 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstaceState){
-        return inflater.inflate(R.layout.account_summary,null,false);
+        View v = inflater.inflate(R.layout.account_summary,null,false);
+
+        Button sms = v.findViewById(R.id.sms);
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openSMS(getContext(),"Hey there","1234321234");
+
+            }
+        });
+
+        Button geo = v.findViewById(R.id.geoLocation);
+
+        geo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri ma = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway%2C+CA");
+                showMap(ma);
+            }
+        });
+        return v;
     }
 
+
+    public static void openSMS(Context context, String smsBody, String tel) {
+        Uri uri = Uri.parse("smsto:" + tel);
+        Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+        it.putExtra("sms_body", smsBody);
+        context.startActivity(it);
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
